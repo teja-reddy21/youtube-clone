649c097b893a438bdcfec9634eef7e415288603c
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-05-31 20:27:38 +0530

    home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.css'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Feed from '../../components/Feed/Feed'
 const Home = ({sidebar}) => {
+
+   const [category,setCategory]=useState(0)
   return (
     <div>
-      <Sidebar sidebar={sidebar}/>
+      <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
       <div className={`container ${sidebar?"":'large-container'}`}>
-        <Feed/>
+        <Feed category={category}/>
       </div>
     </div>
   )
