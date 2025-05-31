297f9a6a941e3ab7402a0c47c008d6953e781032
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-05-31 20:28:44 +0530

    nav bar

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -3,10 +3,12 @@ import './video.css'
 
 import PlayVideo from '../../components/PlayVideo/PlayVideo'
 import Recommended from '../../components/Recommended/Recommended'
+import { useParams } from 'react-router-dom'
 const Video = () => {
+  const {videoId,categoryId}=useParams();
   return (
     <div className='play-container'>
-     <PlayVideo/>
+     <PlayVideo videoId={videoId} />
      <Recommended/>
     </div>
   )
