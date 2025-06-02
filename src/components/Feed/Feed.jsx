df3033517088f1343124d8a57c7d36d16e7dff62
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-06-02 16:56:02 +0530

    feeded the video data

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './feed.css'
 
 import thumbnail1 from '../../assets/thumbnail1.png'
@@ -10,106 +10,33 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import {Link} from 'react-router-dom'
+import { API_KEY, value_converter } from '../../Data'
+const Feed = ({category}) => {
 
-const Feed = () => {
+  const [data,setData]=useState([])
+
+  const fetchData=async()=>{
+    const videoList_url=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+    await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[category])
   return (
     <div className="feed">
-    <Link to={`video/20/4521`} className='card'>
-       <img src={thumbnail1} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
+      {data.map((item,index)=>{
+          return(
+  <Link to={`video/${item.snippet.categoryId}/${item.id}`} className='card'>
+       <img src={item.snippet.thumbnails.medium.url} alt=''/>
+       <h2>{item.snippet.title}</h2>
+       <h3>{item.snippet.channeltitle}</h3>
+       <p>{value_converter(item.statistics.viewCount)} views &bull; {item.snippet.publishedAt}</p>
     </Link>
-     <div className='card'>
-       <img src={thumbnail2} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail3} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail4} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail5} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail6} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail7} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail8} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-      <div className='card'>
-       <img src={thumbnail1} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail2} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail3} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail4} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail5} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail6} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail7} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
-     <div className='card'>
-       <img src={thumbnail8} alt=''/>
-       <h2>Best channel to learn coding that help you to be a web developer</h2>
-       <h3>Cooking with Teju</h3>
-       <p>15k views &bull; 2 days ago</p>
-    </div>
+          )
+      })}
+  
+     
     </div>
 
   )
